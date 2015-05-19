4679e0f05920840cd1aeb045795ee41d4cd999cb
david <dskowronek.ds@gmail.com>
2015-05-19 14:07:26 +0200

    model User complete

--- Code/buzzit/buzzit/settings.py
+++ Code/buzzit/buzzit/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'buzzit_models'
 )
 
 MIDDLEWARE_CLASSES = (
--- Code/buzzit/buzzit_models/models.py
+++ Code/buzzit/buzzit_models/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Profile(User):
+class User(User):
     profile_picture = models.URLField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
-    gender = models.CharField(blank=True, null=True)
+    gender = models.CharField(blank=True, null=True, max_length=1)
