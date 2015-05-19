c347d5e61367d066837785cbda74cca6f1065e02
david <dskowronek.ds@gmail.com>
2015-05-19 13:41:35 +0200

    model User

--- Code/buzzit/buzzit_models/models.py
+++ Code/buzzit/buzzit_models/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(User):
-    """
-
-    """
     profile_picture = models.URLField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    gender = models.CharField(blank=True, null=True)
