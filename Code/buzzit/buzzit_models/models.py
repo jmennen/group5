bb5b78bf71777737fe31a18a081ec8453fd81b0e
david <dskowronek.ds@gmail.com>
2015-05-20 21:04:59 +0200

    got Profile again and one field "user" as a connection to user

--- Code/buzzit/buzzit_models/models.py
+++ Code/buzzit/buzzit_models/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class User(User):
+class Profile(models.Model):
+    user = models.OneToOneField(User, primary_key=True)
     profile_picture = models.URLField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     gender = models.CharField(blank=True, null=True, max_length=1)
