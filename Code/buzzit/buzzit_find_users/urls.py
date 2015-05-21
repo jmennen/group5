276e1587932b3491f992d5e35fbcc919064bc88d
david <dskowronek.ds@gmail.com>
2015-05-21 15:25:28 +0200

    deleted all files that were needed for json comm

--- Code/buzzit/buzzit_find_users/urls.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'David'
-
-from django.conf.urls import include, url
-from . import views
-
-urlpatterns = [
-    url(r'^users/(?P<username>[a-zA-Z0-9]+)/', views.find_users, name="find_user"),
-]
