f4b3e8822e5725cfcdf8a48e795928e246f77c7d
david <dskowronek.ds@gmail.com>
2015-05-21 11:43:58 +0200

    working on find_users view

--- Code/buzzit/buzzit_find_users/views.py
+++ Code/buzzit/buzzit_find_users/views.py
@@ -1,12 +1,13 @@
 from django.http import JsonResponse
+from django.contrib.auth.models import User
 
 
 def find_users(request, username):
     if not request.user.is_authenticated():
         error = {"ok": False, "error": "login first"}
         return JsonResponse(error)
-    if request.method == "POST":
-
-        return JsonResponse({"ok" : False, "error" : "not implemented now"})
-    error = {"ok": False, "error": "wrong method - only POST allowed here"}
+    if request.method == "GET":
+        users = User.objects.filter(username__like=username)
+        return JsonResponse({"ok" : True, "users" : users})
+    error = {"ok": False, "error": "wrong method - only GET allowed here"}
     return JsonResponse(error)
