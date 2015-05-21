3309a915ba68fbd0eaa9faba37393c1fb268b631
david <dskowronek.ds@gmail.com>
2015-05-21 11:20:32 +0200

    url for update get & update view

--- Code/buzzit/buzzit_user_profile/urls.py
+++ Code/buzzit/buzzit_user_profile/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^get/(?P<username>[a-zA-Z0-9]+)/', views.get_profile_info, name="get_profile")
+    url(r'^get/(?P<username>[a-zA-Z0-9]+)/', views.get_profile_info, name="get_profile"),
+    url(r'^update/', views.update_profile_info, name="update_profile")
 ]
--- Code/buzzit/buzzit_user_profile/views.py
+++ Code/buzzit/buzzit_user_profile/views.py
@@ -1,40 +1,79 @@
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth import user_logged_in
 from django.core import serializers
 from django.http import JsonResponse, HttpResponse
-import logging
 from buzzit_models.models import Profile
-
-
-def login_required_json(self):
-    if not user_logged_in:
-        error = {"error": "login first"}
-        return JsonResponse(error)
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+from django.forms.models import model_to_dict
 
 def get_profile_info(request, username):
+    """
+    GET the requested profile.
+    The user is also returned instead of only a foreign key, but some fields are excluded:
+    excluded = ["password", "user_permissions", "groups", "email"]
+    Args:
+        request: the http request
+        username: the username being requested
+    Return:
+        JSON response in two ways
+        1.  {"ok" : False, "error" : "<error message>"} if any error occured
+        2.  {"ok" : True, "profile" :   {
+                                            <profile object>,
+                                            ...,
+                                            "user" : <user object without excluded fields>,
+                                            ...
+                                        }
+            }
+    """
+    if not request.user.is_authenticated():
+        error = {"ok": False, "error": "login first"}
+        return JsonResponse(error)
     if (request.method == "GET"):
-        user = User.objects.all()
-        if len(user) < 1:
-            logging.getLogger(__name__).error(user)
-            error = {"error": "profile " + username + " not found"}
+        try:
+            user = User.objects.get(username=username)
+        except ObjectDoesNotExist:
+            error = {"ok": False, "error": "user " + username + " not found"}
             return JsonResponse(error)
-        user.password = "private"
-        return JsonResponse(user, safe=False)
-    error = {"error": "wrong method - only GET allowed here"}
+        try:
+            profile_obj = Profile.objects.get(user=user)
+        except ObjectDoesNotExist:
+            error = {"ok": False, "error": "profile " + username + " not found"}
+            return JsonResponse(error)
+        profile_dict = model_to_dict(profile_obj)
+        user_dict = model_to_dict(user, exclude=["password", "user_permissions", "groups", "email"])
+        profile_dict["user"] = user_dict
+        ret = {"ok": True, "profile": profile_dict}
+        return JsonResponse(ret, content_type="application/json")
+    error = {"ok": False, "error": "wrong method - only GET allowed here"}
     return JsonResponse(error)
 
 
-def update_profile_info(request, username):
+def update_profile_info(request):
+    """
+    UPDATE the profile. This contains the possibility to update the profile description and gender.
+    Args:
+        request: the http request
+    Return:
+        JSON response in two ways
+        1. {"ok" : False, "error" : "<error message>"} if any error occured
+        2. {"ok" : True, "info" : {"updated" : <updated keys as array>, "ignored" : <ignored keys>}}
+    """
+    if not request.user.is_authenticated():
+        error = {"ok": False, "error": "login first"}
     if request.method == "POST":
-        user = User.objects.filter(username=username)
-        update_str = request.POST.get("update", False)
-        if not update_str:
-            error = {"error": "no update data given"}
+        if not request.POST:
+            error = {"ok": False, "error": "no update data given", "post": request.POST}
             return JsonResponse(error)
-        update = serializers.deserialize('json', update_str)
-        for key in update:
-            setattr(user, key, update[key])
-        updatable_values = ["description", "gender", "first_name", "last_name"]
-        user.save(force_update=True, update_fields=updatable_values)
-    error = {"error": "wrong method - only POST allowed here"}
+        user = request.user;
+        profile = Profile.objects.filter(user=user)[0]
+        updatable_values = ["description", "gender"]
+        info = {"updated": [], "ignored": []}
+        for key in request.POST:
+            if key in updatable_values:
+                info["updated"].append(key)
+                setattr(profile, key, request.POST[key])
+            else:
+                info["ignored"].append(key)
+        profile.save(force_update=True)
+        return JsonResponse({"ok": True, "info": info})
+    error = {"ok": False, "error": "wrong method - only POST allowed here"}
     return JsonResponse(error)
