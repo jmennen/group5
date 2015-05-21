7af45f55922538a9749b7773ff29a2918fc6e6cb
david <dskowronek.ds@gmail.com>
2015-05-21 11:55:44 +0200

    changed sub url of user finding to "users", find users works now

--- Code/buzzit/buzzit_find_users/urls.py
+++ Code/buzzit/buzzit_find_users/urls.py
@@ -4,5 +4,5 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^user/(?P<username>[a-zA-Z0-9]+)/', views.find_users(), name="find_user"),
+    url(r'^users/(?P<username>[a-zA-Z0-9]+)/', views.find_users, name="find_user"),
 ]
--- Code/buzzit/buzzit_find_users/views.py
+++ Code/buzzit/buzzit_find_users/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 from django.contrib.auth.models import User
+from django.forms.models import model_to_dict
 
 
 def find_users(request, username):
@@ -7,7 +8,10 @@ def find_users(request, username):
         error = {"ok": False, "error": "login first"}
         return JsonResponse(error)
     if request.method == "GET":
-        users = User.objects.filter(username__like=username)
-        return JsonResponse({"ok" : True, "users" : users})
+        users = User.objects.filter(username__contains=username)
+        users_arr = []
+        for user in users:
+            users_arr.append(model_to_dict(user, exclude=["password", "user_permissions", "groups", "email"]))
+        return JsonResponse({"ok": True, "users": users_arr})
     error = {"ok": False, "error": "wrong method - only GET allowed here"}
     return JsonResponse(error)
