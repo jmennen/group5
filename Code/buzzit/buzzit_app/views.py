0492f43a643f18974ac232a2acc3120701cf3ccb
david <dskowronek.ds@gmail.com>
2015-05-21 18:27:00 +0200

    in settings.py added buzzit_app used
    generates views for registering, viewing a profile, editing a profile, editing userdata and displaying search results

--- Code/buzzit/buzzit_app/views.py
+++ Code/buzzit/buzzit_app/views.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.views.generic import DetailView, UpdateView, ListView
+from buzzit_models.models import *
+from django.contrib.auth.forms import UserCreationForm
+from django.core.urlresolvers import reverse
 
-# Create your views here.
+@login_required
+class ProfileView(DetailView):
+    model = Profile
+
+@login_required
+class EditProfileView(UpdateView):
+    model = Profile
+
+@login_required
+class EditUserdataView(UpdateView):
+    model = User
+
+@login_required
+class UserSearchResultsView(ListView):
+    model = User
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('start'))
+    else:
+        form = UserCreationForm()
+    return render(request, "guest/register.html", {'form' : form})
