6b6cac586cfbeabff5c65dc65862fad12b33d54c
david <dskowronek.ds@gmail.com>
2015-05-21 11:58:31 +0200

    added documentation

--- Code/buzzit/buzzit_find_users/views.py
+++ Code/buzzit/buzzit_find_users/views.py
@@ -4,6 +4,20 @@ from django.forms.models import model_to_dict
 
 
 def find_users(request, username):
+    """
+    GET the users, whose username contains <username>
+    excluded = ["password", "user_permissions", "groups", "email"]
+    Args:
+        request: the http request
+        username: the (part of the) username of users to find
+    Return:
+        JSON response in two ways
+        1.  {"ok" : False, "error" : "<error message>"} if any error occured
+        2.  {"ok" : True, "users" :   [
+                                            <user object without excluded fields>
+                                        ]
+            }
+    """
     if not request.user.is_authenticated():
         error = {"ok": False, "error": "login first"}
         return JsonResponse(error)
