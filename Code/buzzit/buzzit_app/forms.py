dbb2214c6de60ec0e9deec59f78fa713d3cbe2f2
tryToCode <tryToCode@users.noreply.github.com>
2015-05-28 21:58:27 +0200

    Update forms.py

--- Code/buzzit/buzzit_app/forms.py
+++ Code/buzzit/buzzit_app/forms.py
@@ -2,11 +2,10 @@ from django import forms
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 
-#form to display the user registration, check if the username already exists or password don't match 
-#and throw errors to interact with user
+#form to display user registration 
 
 class RegistrationForm(forms.Form):
-    # form fields for the user registration, use widget to style the class widget with different attributes
+    # form fields for user registration, use widget class which give us the opportunity to customize form field with different attributes under circumstances
     
     username = forms.RegexField(regex=r'^\w+$', widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Username"), error_messages={ 'invalid': _("This value must contain only letters, numbers and underscores.") })
     email = forms.EmailField(widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Email address"))
@@ -16,17 +15,17 @@ class RegistrationForm(forms.Form):
     password2 = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), label=_("Password (again)"))
 
     def clean_username(self):
-        # cleaning method that operates on the form field username
+        # cleaning method that operates on the form field username, default case username is not taken
         # check if the given username exists already, in this case throw a message, when not, store the username
         try:
             user = User.objects.get(username__iexact=self.cleaned_data['username'])
         except User.DoesNotExist:
             return self.cleaned_data['username']
-        raise forms.ValidationError(_("Der Benutzername existiert bereits. versuch bitte einen Anderen."))
+        raise forms.ValidationError(_("Der Benutzername existiert bereits."))
 
     def clean(self):
-        # cleaning method that operates on the form fields password and password again
-        # check if the passwords valid are, when the given passwords don't match, throw a error
+        # cleaning method that operates on the form fields password 
+        # check if the passwords are valid, and when the given passwords don't match, throw a error
         if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
             if self.cleaned_data['password1'] != self.cleaned_data['password2']:
                 raise forms.ValidationError(_("Die Passworte stimmen leider nicht überein."))
