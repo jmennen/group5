018af12a9a51b2822165f3b28eaf5735670454d6
david <dskowronek.ds@gmail.com>
2015-06-02 07:14:46 +0200

    models created:   Circle   Circle_message   Message
    added all models to admin.py

--- Code/buzzit/buzzit_models/models.py
+++ Code/buzzit/buzzit_models/models.py
@@ -6,3 +6,19 @@ class Profile(models.Model):
     profile_picture = models.URLField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     gender = models.CharField(blank=True, null=True, max_length=1)
+
+
+class Circle(models.Model):
+    owner = models.ForeignKey(Profile, blank=False, null=False) # User erstellt Kreis
+    messages = models.ManyToManyField(Message, blank=True, null=True) # Kreis enthält Nachrichten
+    members = models.ManyToManyField(User, blank=True, null=True) # Kreis enthält User
+
+
+class Message(models.Model):
+    creator = models.ForeignKey(User, blank=False, null=False)
+    created = models.DateTimeField(blank=False, null=False)
+    text = models.CharField(blank=False, null=False, max_length=140)
+
+
+class Circle_message(Message):
+    answer_to = models.ForeignKey("self", blank=True, null=True, related_name="answer_to") # ist Antwort auf Kreisnachricht
