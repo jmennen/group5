a9223f90ceb4a1d312338b133857e39e9485e989
david <dskowronek.ds@gmail.com>
2015-06-02 22:33:06 +0200

    in Circle changed M2M-Field to Message to a   M2M-Field to Cirlce_message

--- Code/buzzit/buzzit_models/models.py
+++ Code/buzzit/buzzit_models/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, primary_key=True)
     profile_picture = models.URLField(blank=True, null=True)
@@ -14,12 +15,12 @@ class Message(models.Model):
     text = models.CharField(blank=False, null=False, max_length=140)
 
 
-class Circle(models.Model):
-    owner = models.ForeignKey(User, blank=False, null=False, related_name="owner_of_circle") # User erstellt Kreis
-    messages = models.ManyToManyField(Message) # Kreis enthält Nachrichten
-    members = models.ManyToManyField(User) # Kreis enthält User
-    name = models.CharField(max_length=40, null=False, blank=False)
-
-
 class Circle_message(Message):
-    answer_to = models.ForeignKey("self", blank=True, null=True) # ist Antwort auf Kreisnachricht
+    answer_to = models.ForeignKey("self", blank=True, null=True)  # ist Antwort auf Kreisnachricht
+
+
+class Circle(models.Model):
+    owner = models.ForeignKey(User, blank=False, null=False, related_name="owner_of_circle")  # User erstellt Kreis
+    messages = models.ManyToManyField(Circle_message)  # Kreis enthält Nachrichten
+    members = models.ManyToManyField(User)  # Kreis enthält User
+    name = models.CharField(max_length=40, null=False, blank=False)
