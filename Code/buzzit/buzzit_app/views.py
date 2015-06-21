379587de62bf003428e8976fa9a05d47a0ca0bfb
david <dskowronek.ds@gmail.com>
2015-06-21 11:41:23 +0200

    switched to sha1

--- Code/buzzit/buzzit_app/views.py
+++ Code/buzzit/buzzit_app/views.py
@@ -395,9 +395,9 @@ def reset_password(request):
         except ObjectDoesNotExist:
             return render(request, "forgot_password/forgot_password.html",
                           {"errors": "Benutzername oder Email stimmen nicht"})
-        new_pwd = hashlib.md5()
+        new_pwd = hashlib.sha1()
         new_pwd.update(urandom(64))
-        new_pwd = new_pwd.digest()
+        new_pwd = new_pwd.hexdigest()
         user.set_password(new_pwd)
         user.save()
         send_mail("Dein neues Password",
