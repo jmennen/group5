6306ad26d701160b442b65d4dfeb5528fb651b27
david <dskowronek.ds@gmail.com>
2015-06-23 21:54:22 +0200

    changed directmessaging

--- Code/buzzit/buzzit_messaging/views.py
+++ Code/buzzit/buzzit_messaging/views.py
@@ -477,7 +477,7 @@ def direct_messages_overview(request):
         conversation = Directmessage.objects.filter(creator__username="SYSTEM", receiver=request.user).order_by("created")
     return render(request, "buzzit_messaging/logged_in/direct_messages.html",
                   {
-                      "chats_sorted" : list(chats).sort(),
+                      "chats_sorted" : list(chats),
                       "chats": chats,
                       "chatsMsgCount": chatsMsgCount,
                       "active_conversation_partner": active_conversation_partner,
