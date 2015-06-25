d7523a4265a9d00705c57ce2add3d1a886798e27
david <dskowronek.ds@gmail.com>
2015-06-25 13:19:47 +0200

    updated messagefilter to show symbol

--- Code/buzzit/buzzit_app/templatetags/messagefilters.py
+++ Code/buzzit/buzzit_app/templatetags/messagefilters.py
@@ -14,12 +14,12 @@ def messagefilter(message):
     #parse mentions
     for mention in message.mentions.all():
         link = reverse("view_profile", args=(mention.pk,))
-        link = "<a href='%s'>%s</a>" % (link, mention.username)
+        link = "<a href='%s'>@%s</a>" % (link, mention.username)
         message_text = re.sub(r"\b%s\b" % mention.username, link, message_text)
     #parse themes
     for theme in message.themes.all():
         link = reverse("search_theme", args=(theme.pk,))
-        link = "<a href='%s'>%s</a>" % (link, theme.name)
+        link = "<a href='%s'>#%s</a>" % (link, theme.name)
         message_text = re.sub(r"\b%s\b" % theme.name, link, message_text)
     return message_text
 
