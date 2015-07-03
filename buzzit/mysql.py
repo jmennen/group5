bc963ee58739c4d3eb22946400cec410d98df63a
jmennen <janmennen@gmx.de>
2015-07-03 16:04:21 +0200

    Heroku ready now

---
+++ buzzit/mysql.py
@@ -0,0 +1,11 @@
+def get_db_config():
+    return {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'sn',
+            'USER': 'sn',
+            'PASSWORD': 'UniAOSJOPEhS1PFFQaUyH4ftyhkUIOKH',
+            'HOST': 'vps146949.ovh.net',  # Or an IP Address that your DB is hosted on
+            'PORT': '3306',
+        }
+    }
