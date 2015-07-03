7334fa33eb0dd2719f20b6de0a0d41f8c90434e4
jmennen <janmennen@gmx.de>
2015-07-03 16:50:09 +0200

    heroku changes

--- buzzit/wsgi.py
+++ buzzit/wsgi.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "buzzit.settings")
-
-application = get_wsgi_application()
